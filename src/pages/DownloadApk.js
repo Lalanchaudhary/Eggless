fe232dbffc5a311fd6ac062ccbacd96279db6449
Lalan Chaudhary <chaudharylalan28@gmail.com>
2025-07-01 02:18:07 +0530

    app update

--- src/pages/DownloadApk.js
+++ src/pages/DownloadApk.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import eggless from '../assets/Egglesscake.png'
-const APK_URL = 'https://apk-download-site-afd7iueb3-lalan-chaudharys-projects.vercel.app/EgglessCake.apk';
+const APK_URL = 'https://apk-download-site-j6rotsyja-lalan-chaudharys-projects.vercel.app/EgglessCake.apk';
 
 const DownloadApk = () => {
   return (
